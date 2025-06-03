68841d8499cce760bb4c2c2d731908163f45b953
Damieee <joshezekiel.dev@gmail.com>
2025-06-03 07:18:58 +0100

    Add database service for message storage and retrieval; implement async methods for adding and fetching messages

---
+++ api/services/database_service.py
@@ -0,0 +1,73 @@
+import asyncio
+from concurrent.futures.thread import ThreadPoolExecutor
+from contextlib import asynccontextmanager
+from dataclasses import dataclass
+from functools import partial
+from pathlib import Path
+from typing import Any, Callable, AsyncIterator, TypeVar
+from typing_extensions import LiteralString, ParamSpec
+import sqlite3
+
+from pydantic_ai.messages import ModelMessage, ModelMessagesTypeAdapter
+
+P = ParamSpec('P')
+R = TypeVar('R')
+
+@dataclass
+class Database:
+    con: sqlite3.Connection
+    _loop: asyncio.AbstractEventLoop
+    _executor: ThreadPoolExecutor
+
+    @classmethod
+    @asynccontextmanager
+    async def connect(cls, file: Path = Path(__file__).parent / '.chat_messages.sqlite') -> 'Database':
+        loop = asyncio.get_event_loop()
+        executor = ThreadPoolExecutor(max_workers=1)
+        con = await loop.run_in_executor(executor, cls._connect, file)
+        return cls(con, loop, executor)
+
+    async def close(self):
+        await self._asyncify(self.con.close)
+        self._executor.shutdown(wait=True)
+
+    @staticmethod
+    def _connect(file: Path) -> sqlite3.Connection:
+        con = sqlite3.connect(str(file))
+        cur = con.cursor()
+        cur.execute('CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, message_list TEXT);')
+        con.commit()
+        return con
+
+    async def add_messages(self, messages: bytes):
+        await self._asyncify(
+            self._execute,
+            'INSERT INTO messages (message_list) VALUES (?);',
+            messages,
+            commit=True,
+        )
+        await self._asyncify(self.con.commit)
+
+    async def get_messages(self) -> list[ModelMessage]:
+        c = await self._asyncify(
+            self._execute, 'SELECT message_list FROM messages order by id'
+        )
+        rows = await self._asyncify(c.fetchall)
+        messages: list[ModelMessage] = []
+        for row in rows:
+            messages.extend(ModelMessagesTypeAdapter.validate_json(row[0]))
+        return messages
+
+    def _execute(self, sql: LiteralString, *args: Any, commit: bool = False) -> sqlite3.Cursor:
+        cur = self.con.cursor()
+        cur.execute(sql, args)
+        if commit:
+            self.con.commit()
+        return cur
+
+    async def _asyncify(self, func: Callable[P, R], *args: P.args, **kwargs: P.kwargs) -> R:
+        return await self._loop.run_in_executor(
+            self._executor,
+            partial(func, **kwargs),
+            *args,
+        )
